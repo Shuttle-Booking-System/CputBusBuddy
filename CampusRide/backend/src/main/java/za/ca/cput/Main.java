2327ca9397470ea8d26a2fa21e2702eec4d4090c
LuyandaMbalenhle <144934497+LuyandaMbalenhle@users.noreply.github.com>
2025-08-07 17:36:22 +0200

    init comit backend

---
+++ CampusRide/backend/src/main/java/za/ca/cput/Main.java
@@ -0,0 +1,10 @@
+package za.ca.cput;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
